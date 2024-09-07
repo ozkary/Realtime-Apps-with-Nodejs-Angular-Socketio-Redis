8825e7782902e0ee9260163396546dee64f8ee10
ozkary <ozkary@gmail.com>
2024-09-07 12:41:24 -0400

    update server dependencies

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@
     *
     * Update/Fix History
     *   ogarcia 01/20/2018 initial implementation
+    *   ogarcia 09/07/2024 update to latest dependencies
     *
     */
 /*
@@ -22,67 +23,64 @@
  * use npm isntall from the server folder to download all the dependencies
  */
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var server = require('http').createServer(app);
-var device  = require('express-device');
+// core modules
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
+import device from 'express-device';
 
-app.configure(function(){
-	// I need to access everything in '/public' directly
-	app.use(express.static(__dirname + '/public'));
-	//set the view engine
-	app.set('view engine', 'ejs');
-	app.set('views', __dirname +'/views');
-	app.use(device.capture());
-});
+// app modules
+import config from './modules/config.js';
+import headers from './modules/headers.js';
+import error from './modules/error-handler.js';
+import api from './modules/telemetry-api.js';
+import strategy from './data_modules/strategy';
+import socket from './modules/socketio.js';
 
+const app = express();
+const server = createServer(app);
+
+// Middleware and Configuration
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname +'/views');
+app.use(device.capture());
 app.use(bodyParser.json());
 
-//include required modules
-var config = require('./modules/config.js');            //configuration
-var headers = require('./modules/headers.js');		      //access-control headers
-var error = require('./modules/error-handler.js');		  //error handler
-var api = require('./modules/telemetry-api.js');		    //apis
 
 //TODO repository strategy
 //step 1 use sql server repo
 //step 2 add the realtime socket lib
 //step 3 add redis cache
 //step 4 add message broker
-const strategy = require('./data_modules/strategy');
 const repository = strategy.broker;
-// initialize the repo for the broker only
-repository.init(strategy.redis, strategy.sql); // add params for the broker
 
-//realtime socket integration
-const socket = require('./modules/socketio.js');		      //socketio module
+// initialize the repo for the broker only with redis and sql
+repository.init(strategy.redis, strategy.sql);
 socket.init(server, config, repository);
-
-//initialize modules
 headers.init(app);
-api.init(app, repository);                             //api routes
-error.init(app);                                        //enable error handling
+api.init(app, repository);
+error.init(app);
 
-//js app to act as a device
-app.get("/", function(req, res){
-  res.render('index', {}); 
+// Routes
+app.get("/", (req, res) => {
+  res.render('index', {});
 });
 
-//start the server app
-
+// Start the Server
 const APP_PORT = config.PORT;
-app.listen(APP_PORT, function () {
-  
+server.listen(APP_PORT, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log("Node.js Realtime Data App by ozkary.com listening at http://%s:%s", host, port);
-  console.log("Open a browser and type the server address including the port");
-  console.log("The angular client runs on localhost:4200");
-  console.log("The socket client and server run on localhost:" + `${config.SOCKET.port}`);
-  console.log("The API server run on localhost:"+ `${config.PORT}`);
-  console.log("The test app runs on localhost:"+ `${config.SOCKET.port}`);
-  console.log("Redis server run on localhost:"+ `${config.REDIS.port}`);
-  console.log("Redis channel name: telemetry:data");
+  console.log(`Node.js Realtime Data App by ozkary.com listening at http://${host}:${port}`);
+  console.log(`Open a browser and type the server address including the port`);
+  console.log(`The angular client runs on localhost:4200`);
+  console.log(`The socket client and server run on localhost:${config.SOCKET.port}`);
+  console.log(`The API server runs on localhost:${config.PORT}`);
+  console.log(`The test app runs on localhost:${config.SOCKET.port}`);
+  console.log(`Redis server runs on localhost:${config.REDIS.port}`);
+  console.log(`Redis channel name: telemetry:data`);
 });
+
+
