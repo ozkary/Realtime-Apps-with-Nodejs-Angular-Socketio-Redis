f954373018b33d74c699b50e576a5bf436d8ab97
ozkary <ozkary@gmail.com>
2024-09-07 11:34:12 -0400

    update angular 18

--- client/src/app/features/telemetry/telemetry.module.ts
+++ client/src/app/features/telemetry/telemetry.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 //TODO change provider on telemetry.component.ts
 import { TelemetryService } from './telemetry.factory';
 import { TelemetryComponent } from './telemetry.component';
 
-@NgModule({
-  imports: [
-    CommonModule, HttpClientModule
-  ],
-  providers: [TelemetryService],
-  declarations: [TelemetryComponent]
-})
+@NgModule({ declarations: [TelemetryComponent], imports: [CommonModule], providers: [TelemetryService, provideHttpClient(withInterceptorsFromDi())] })
 export class TelemetryModule { }
